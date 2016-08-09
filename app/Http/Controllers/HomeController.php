2eb192bc4e19384b5e6fcf8f2ba1c87c501805b2
huy <huyanhit@gmail.com>
2016-08-09 08:29:11 +0700

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,19 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Library\myfunction;
+use App\Http\Models\HeaderlineModel;
 
 class HomeController extends Controller
 {
     
     public function __construct()
     {
+        $this->headerline = new HeaderlineModel();
         $this->index();
     }
     
     public function index()
     {
-        $myFunction = new myFunction;
-        $data[] = array();
+        $data = $this->headerline->getAll();
         return view("home",$data);
     }
 }
