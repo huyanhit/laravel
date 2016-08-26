a05ad93f707547532b080c4ee8d28e745052852f
huyanhit <xulanh00@gmail.com>
2016-08-26 12:00:53 +0700

    up

--- resources/views/include/sidebar-right.blade.php
+++ resources/views/include/sidebar-right.blade.php
@@ -1,85 +1,86 @@
-<div class="siderbar-news">
-	<ul class="sidr-class-nav">
-		<li><a href="index.html"><img src="images/home.png" alt="Magazine"></a></li>
-		<li class="sidr-class-dropdown"><a href="index.html" class="sidr-class-dropdown-toggle sidr-class-disabled">Home<b class="sidr-class-caret"></b></a>
-			<ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-				<li><a href="magazine2.html">Homepage 2</a></li>
-				<li><a href="blog.html">Homepage 3</a></li>
-				<li><a href="shop-page.html">Homepage 4</a></li>
-			</ul>
-		</li>
-		<li><a href="about.html">About</a></li>
-		<li class="sidr-class-dropdown"><a href="##" class="sidr-class-dropdown-toggle sidr-class-disabled">Page Templates<b class="sidr-class-caret"></b></a>
-			<ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-                <li><a href="index.html">Magazine 1</a></li>
-				<li><a href="magazine2.html">Magazine 2</a></li>
-				<li><a href="shop-page.html">Magazine 3</a></li>
-				<li class="sidr-class-dropdown"><a href="#" class="sidr-class-dropdown-toggle sidr-class-disabled">Blog Page<b class="sidr-class-caret"></b></a>
-					<ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-						<li><a href="blog.html">Blog</a></li>
-						<li><a href="blog-left.html">Blog Left</a></li>
-						<li><a href="blog-right.html">Blog Right</a></li>
-						<li><a href="single-post.html">Single Blog</a></li>
-					</ul>
-				</li>
-                <li class="sidr-class-dropdown"><a href="#" class="sidr-class-dropdown-toggle sidr-class-disabled">Shop Page<b class="sidr-class-caret"></b></a>
-					<ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-						<li><a href="shop-page.html">Shop</a></li>
-						<li><a href="shop-detail.html">Shop Detail</a></li>
-						<li><a href="cart.html">Cart Page</a></li>
-						<li><a href="checkout.html">Checkout Page</a></li>
-						<li><a href="account.html">Account Page</a></li>
-					</ul>
-				</li>
-                <li><a href="fullwidth.html">Fullwidth</a></li>
-                <li><a href="features.html">Features</a></li>
-				<li><a href="contact.html">Contact</a></li>
-                <li><a href="author.html">Author Pages</a></li>
-				<li><a href="404.html">Error 404</a></li>
-				<li><a href="fullwidth.html">Full Width</a></li>
-				<li><a href="gallery-2col.html">Galleries – 2 Columns</a></li>
-				<li><a href="gallery-3col.html">Galleries – 3 Columns</a></li>
-				<li><a href="gallery-4col.html">Galleries – 4 Columns</a></li>
-				<li><a href="sitemap.html">Site Map</a></li>
-			</ul>
-		</li>
-        <li class="sidr-class-dropdown"><a href="##" class="sidr-class-dropdown-toggle sidr-class-disabled">Shortcodes<b class="sidr-class-caret"></b></a>
-			<ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-				<li><a href="accordion.html">Accordion</a></li>
-				<li><a href="blockquotes.html">Blockquotes</a></li>
-				<li><a href="button.html">Buttons</a></li>
-				<li><a href="column.html">Columns</a></li>
-				<li><a href="dropcap.html">Dropcaps</a></li>
-				<li><a href="heading.html">Heading</a></li>
-				<li><a href="tabs.html">Tabs</a></li>
-				<li><a href="tables.html">Tables</a></li>
-				<li><a href="warning.html">Warning</a></li>
-			</ul>
-		</li>
-		<li class="sidr-class-dropdown"><a href="blog.html" class="sidr-class-dropdown-toggle sidr-class-disabled">Blog<b class="sidr-class-caret"></b></a>
-			<ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-				<li><a href="fashion-news.html">Fashion News</a></li>
-				<li><a href="#">Game News</a></li>
-				<li><a href="#">Sport News</a></li>
-				<li class="sidr-class-dropdown"><a href="#" class="sidr-class-dropdown-toggle sidr-class-disabled">Technology<b class="sidr-class-caret"></b></a>
-					<ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-						<li><a href="fashion-news.html">Fashion News</a></li>
-						<li><a href="#">Game News</a></li>
-						<li><a href="#">Sport News</a></li>
-						<li><a href="#">Technology</a></li>
-					</ul>
-				</li>
-			</ul>
-		</li>
-		<li class="sidr-class-dropdown"><a href="#" class="sidr-class-dropdown-toggle sidr-class-disabled">Shop Page<b class="sidr-class-caret"></b></a>
-            <ul class="sidr-class-sub-menu sidr-class-dropdown-menu">
-                <li><a href="shop-page.html">Product Page</a></li>
-                <li><a href="shop-detail.html">Product Detail</a></li>
-                <li><a href="cart.html">Cart Page</a></li>
-                <li><a href="checkout.html">checkout</a></li>
-                <li><a href="order.html">Order Detail</a></li>
-            </ul>
-        </li>
-		<li><a href="contact.html">Contact</a></li>
-	</ul>
+<div id="sidebar" class="span4">
+    <div class="widget widget_latestpost">
+        <h3 class="title"><span>Recent Posts</span></h3>
+        <div class="latest-posts widget">
+            <div class="latest-post clearfix">
+                <a href="#"><img width="225" height="136" src="http://placehold.it/225x136" class="thumb fl" alt="" title=""></a>
+                <h4><a href="#" rel="bookmark" title="Lectus non rutrum pulvinar urna leo dignissim lorem">Lectus non rutrum pulvinar urna leo</a></h4>
+                <div class="post-time">August 12, 2013</div>
+                <div class="ratings" style="float: none">
+                    <span class="star-rating-control"><div class="rating-cancel" style="display: none;"><a title="Cancel Rating"></a></div><div role="text" aria-label="" class="star-rating rater-0 star star-rating-applied star-rating-readonly star-rating-on"><a title="1">1</a></div><div role="text" aria-label="" class="star-rating rater-0 star star-rating-applied star-rating-readonly star-rating-on"><a title="2">2</a></div><div role="text" aria-label="" class="star-rating rater-0 star star-rating-applied star-rating-readonly star-rating-on"><a title="3">3</a></div><div role="text" aria-label="" class="star-rating rater-0 star star-rating-applied star-rating-readonly star-rating-on"><a title="4">4</a></div><div role="text" aria-label="" class="star-rating rater-0 star star-rating-applied star-rating-readonly"><a title="5">5</a></div></span><input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-1" value="1" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-1" value="2" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-1" value="3" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-1" value="4" disabled="disabled" checked="checked" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-1" value="5" disabled="disabled" style="display: none;">
+                </div>
+            </div>
+
+            <div class="latest-post clearfix">
+                <a href="#"><img width="225" height="136" src="http://placehold.it/225x136" class="thumb fl" alt="" title=""></a>
+                <h4><a href="#" rel="bookmark" title="Suspen disse auctor dapibus neque pulvinar urna leo">Suspen disse auctor dapibus neque</a></h4>
+                <div class="post-time">August 11, 2013</div>
+                <div class="ratings" style="float: none">
+                    <span class="star-rating-control"><div class="rating-cancel" style="display: none;"><a title="Cancel Rating"></a></div><div role="text" aria-label="" class="star-rating rater-1 star star-rating-applied star-rating-readonly star-rating-on"><a title="1">1</a></div><div role="text" aria-label="" class="star-rating rater-1 star star-rating-applied star-rating-readonly star-rating-on"><a title="2">2</a></div><div role="text" aria-label="" class="star-rating rater-1 star star-rating-applied star-rating-readonly star-rating-on"><a title="3">3</a></div><div role="text" aria-label="" class="star-rating rater-1 star star-rating-applied star-rating-readonly star-rating-on"><a title="4">4</a></div><div role="text" aria-label="" class="star-rating rater-1 star star-rating-applied star-rating-readonly"><a title="5">5</a></div></span><input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-2" value="1" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-2" value="2" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-2" value="3" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-2" value="4" disabled="disabled" checked="checked" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-2" value="5" disabled="disabled" style="display: none;">
+                </div>
+            </div>
+
+            <div class="latest-post clearfix">
+                <a href="#"><img width="225" height="136" src="http://placehold.it/225x136" class="thumb fl" alt="" title=""></a>
+                <h4><a href="#" rel="bookmark" title="Porta lorem ipsum dolor sit amet, consectetur adipiscing risus">Porta lorem ipsum dolor sit amet</a></h4>
+                <div class="post-time">August 2, 2013</div>
+                <div class="ratings" style="float: none">
+                    <span class="star-rating-control"><div class="rating-cancel" style="display: none;"><a title="Cancel Rating"></a></div><div role="text" aria-label="" class="star-rating rater-2 star star-rating-applied star-rating-readonly star-rating-on"><a title="1">1</a></div><div role="text" aria-label="" class="star-rating rater-2 star star-rating-applied star-rating-readonly star-rating-on"><a title="2">2</a></div><div role="text" aria-label="" class="star-rating rater-2 star star-rating-applied star-rating-readonly star-rating-on"><a title="3">3</a></div><div role="text" aria-label="" class="star-rating rater-2 star star-rating-applied star-rating-readonly star-rating-on"><a title="4">4</a></div><div role="text" aria-label="" class="star-rating rater-2 star star-rating-applied star-rating-readonly"><a title="5">5</a></div></span><input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-3" value="1" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-3" value="2" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-3" value="3" disabled="disabled" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-3" value="4" disabled="disabled" checked="checked" style="display: none;">
+                    <input class="star star-rating-applied star-rating-readonly" type="radio" name="recent-post-3" value="5" disabled="disabled" style="display: none;">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="widget widget_latestpost"><h3 class="title"><span>Technology News</span></h3>
+        <div class="latest-posts">
+            <article class="post">
+                <a class="image_thumb_zoom" href="#" title="Permalink to Porta lorem ipsum dolor sit amet, consectetur adipiscing risus" rel="bookmark">
+                <img width="371" height="177" src="http://placehold.it/371x177" alt="">
+                </a>
+                <h4 class="post-title">
+                <a href="#" title="Permalink to Porta lorem ipsum dolor sit amet, consectetur adipiscing risus" rel="bookmark">Porta lorem ipsum dolor sit amet, c...</a>
+                <span class="date">August 2, 2013</span>
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat id augue non dapibus. Morbi ut ipsum cond...</p>
+            </article>
+
+            <article class="post">
+                <div class="entry clearfix">
+                    <a href="#" title="Permalink to Donec consectetuer ligula vulputate sem tristique cursus" rel="bookmark">
+                    <img width="225" height="136" src="http://placehold.it/225x136" class="thumb" alt="">
+                    <h4 class="post-title">Donec consectetuer ligula vulputate...</h4>
+                    </a>
+                    <p>Nam nibh arcu, tristique eget pretium se...</p>
+                    <div class="meta">
+                        <span class="date">July 11, 2013</span>
+                    </div>
+                </div>
+            </article>
+
+            <article class="post">
+                <div class="entry clearfix">
+                    <a href="#" title="Permalink to Quisque sodales viverra ornare vitae libero ac risus" rel="bookmark">
+                    <img width="225" height="136" src="http://placehold.it/225x136" class="thumb" alt="">
+                    <h4 class="post-title">Quisque sodales viverra ornare vita...</h4></a>
+                    <p>Quisque sodales viverra ornare. Aenean p...</p>
+                    <div class="meta">
+                        <span class="date">July 2, 2013</span>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
 </div>
