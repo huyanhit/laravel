8d4ead7891bef5f2d3cd52b50c59ada970d23954
huy <huyanhit@gmail.com>
2016-09-28 22:50:07 +0700

    up

--- resources/views/include/module-contentplaylist.blade.php
+++ resources/views/include/module-contentplaylist.blade.php
@@ -4,7 +4,7 @@
 		//print_r($mutiplaylist);
 		//die;
 	?>
-		<audio width="100%" id="mejs" src="{{Request::root()}}/public/uploads/file/{{$mutiplaylist[0]->file}}" type="audio/mp3" controls="controls">
+		<audio width="100%" id="mejs" src="{{Request::root()}}/public/uploads/file/{{!empty($mutiplaylist[0]->file)?$mutiplaylist[0]->file:''}}" type="audio/mp3" controls="controls">
 			@foreach( $mutiplaylist as $val )
 			<source src="{{Request::root()}}/public/uploads/file/{{$val->file}}" title="{{$val->title}}">
 			@endforeach
