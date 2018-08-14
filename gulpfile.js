9c6c52de04ffe63d8fc421ab4813424236881bcb
huy <huyanhit@gmail.com>
2018-08-14 21:31:18 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,6 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
-    mix.sass('admin.scss');
+    mix.sass('app.scss','resources/assets/css/app.css');
+    mix.sass('admin.scss','app/Modules/Admin/Views/Assets/css');
 });
