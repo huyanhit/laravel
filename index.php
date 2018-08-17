3a2c33120890e9f5a0f0e833567e3b851f05fde9
[Lampart] Le Anh Huy <anh_huy@lampart-vn.com>
2018-08-17 13:27:09 +0700

    up

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -17,7 +19,7 @@
 |
 */
 
-require __DIR__.'/bootstrap/autoload.php';
+require __DIR__.'../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +33,7 @@ require __DIR__.'/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
